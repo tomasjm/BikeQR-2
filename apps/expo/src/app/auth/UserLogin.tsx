2274ac27f487fdb375139633da0a2693dcf568a7
rNNdom <l.galeano01@ufromail.cl>
2023-06-12 03:08:25 -0400

    feature: backend services consumed on aplication

--- apps/expo/src/app/auth/UserLogin.tsx
+++ apps/expo/src/app/auth/UserLogin.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
-import { useForm } from "react-hook-form";
+import { FontAwesome, Foundation } from "@expo/vector-icons";
+import { Controller, useForm } from "react-hook-form";
+
+import { api } from "~/utils/api";
+import { UserRegisterProps } from "./UserRegister";
 
 export default function UserLogin() {
   const {
-    register,
     handleSubmit,
+    control,
+    formState,
+    reset,
     formState: { errors },
-  } = useForm();
+  } = useForm<UserRegisterProps>();
   const router = useRouter();
-  const onSubmit = (data: any) => console.log(data);
+  const userLoginMutation = api.auth.login.useMutation();
+  const onSubmit = (data: any) => {
+    const email = data.email;
+    const password = data.password;
+    userLoginMutation.mutate({ email, password });
+  };
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+      router.push("/home");
+    }
+  }, [formState, reset]);
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="space-y-4">
@@ -27,20 +44,59 @@ export default function UserLogin() {
               Inicio de sesión
             </Text>
           </View>
-          <View className="space-y-2 px-14">
-            <TextInput
-              className="rounded-md border p-3"
-              placeholder="Email"
-              {...register("email", { required: true })}
-            />
-            {errors.exampleRequired && <Text>This field is required</Text>}
-
-            <TextInput
-              className="rounded-md border p-3"
-              placeholder="Contraseña"
-              {...register("password", { required: true })}
-            />
-            {errors.exampleRequired && <Text>This field is required</Text>}
+          <View className="space-y-2 px-8">
+            <View>
+              <Controller
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, value } }) => (
+                  <View className="flex-row items-center space-x-2">
+                    <FontAwesome
+                      name="user"
+                      size={30}
+                      color="black"
+                      style={{ width: 30, height: 30 }}
+                    />
+                    <TextInput
+                      className="flex-1 rounded-md border p-3"
+                      placeholder="Email"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  </View>
+                )}
+                name="email"
+              />
+              {errors.email && (
+                <Text className="text-red-600">Campo requerido</Text>
+              )}
+            </View>
+            <View>
+              <Controller
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, value } }) => (
+                  <View className="flex-row items-center space-x-2">
+                    <Foundation
+                      name="key"
+                      size={30}
+                      color="black"
+                      style={{ width: 30, height: 30 }}
+                    />
+                    <TextInput
+                      className="flex-1 rounded-md border p-3"
+                      placeholder="Contraseña"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  </View>
+                )}
+                name="password"
+              />
+              {errors.password && (
+                <Text className="text-red-600">Campo requerido</Text>
+              )}
+            </View>
           </View>
         </View>
         <View className="items-center pt-5">
--- apps/expo/src/app/auth/UserRegister.tsx
+++ apps/expo/src/app/auth/UserRegister.tsx
@@ -1,43 +1,103 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useForm } from "react-hook-form";
+import { useRouter } from "expo-router";
+import { FontAwesome, Foundation } from "@expo/vector-icons";
+import { Controller, useForm } from "react-hook-form";
 
+import { api } from "~/utils/api";
+
+export interface UserRegisterProps {
+  email: string;
+  password: string;
+}
 export default function UserRegister() {
   const {
-    register,
     handleSubmit,
+    control,
+    formState,
+    reset,
     formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data: any) => console.log(data);
+  } = useForm<UserRegisterProps>();
+  const userRegisterMutation = api.auth.register.useMutation();
+  const onSubmit = (data: UserRegisterProps) => {
+    const email = data.email;
+    const password = data.password;
+    userRegisterMutation.mutate({ email, password });
+  };
+  const router = useRouter();
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+      router.back();
+    }
+  }, [formState, reset]);
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View className="mb-20 space-y-4">
-        <View className="-mb-20 -mt-10 items-center">
+      <View className="space-y-4">
+        <View className="-mb-10 -mt-10 items-center">
           <Image
             style={{ resizeMode: "cover", width: 380, height: 380 }}
             source={require("../../images/BikeQRVectorLogo.png")}
           />
         </View>
         <View className="space-y-5">
-          <View className="-mb-2 items-center">
+          <View className="items-center">
             <Text className="text-2xl font-bold uppercase">Registro</Text>
           </View>
-          <View className="space-y-2 px-14">
-            <TextInput
-              className="rounded-md border p-3"
-              placeholder="Email"
-              {...register("email", { required: true })}
-            />
-            {errors.exampleRequired && <Text>This field is required</Text>}
-
-            <TextInput
-              className="rounded-md border p-3"
-              placeholder="Contraseña"
-              {...register("password", { required: true })}
-            />
-            {errors.exampleRequired && <Text>This field is required</Text>}
+          <View className="space-y-2 px-8">
+            <View>
+              <Controller
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <View className="flex-row items-center space-x-2">
+                    <FontAwesome
+                      name="user"
+                      size={30}
+                      color="black"
+                      style={{ width: 30, height: 30 }}
+                    />
+                    <TextInput
+                      className="flex-1 rounded-md border p-3"
+                      placeholder="Email"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  </View>
+                )}
+                name="email"
+              />
+              {errors.email && (
+                <Text className="text-red-600">Campo requerido</Text>
+              )}
+            </View>
+            <View>
+              <Controller
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, value } }) => (
+                  <View className="flex-row items-center space-x-2">
+                    <Foundation
+                      name="key"
+                      size={30}
+                      color="black"
+                      style={{ width: 30, height: 30 }}
+                    />
+                    <TextInput
+                      className="flex-1 rounded-md border p-3"
+                      placeholder="Contraseña"
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  </View>
+                )}
+                name="password"
+              />
+              {errors.password && (
+                <Text className="text-red-600">Campo requerido</Text>
+              )}
+            </View>
           </View>
         </View>
         <View className="items-center pt-5">
