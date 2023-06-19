b5b35a4b9ade8f536a541b04623d3be25f5a38b3
Tomás Jiménez <tjimenez999@gmail.com>
2023-06-18 23:00:32 -0400

    fix: better code

--- apps/expo/src/components/HomePageList.tsx
+++ apps/expo/src/components/HomePageList.tsx
@@ -3,7 +3,6 @@ import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 
 function HomePageList({ role }: { role: string }) {
-  console.log(role)
   const data = [
     {
       id: 1,
@@ -25,60 +24,43 @@ function HomePageList({ role }: { role: string }) {
       image: require("../images/21936.jpg"),
     },
   ];
-  const router = useRouter();
   return (
     <FlatList
       data={data}
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item }) => {
-        if (item?.role === "ATTENDANT" && role === "ATTENDANT") {
-          return (
-            <TouchableOpacity
-              className="bg-white p-4"
-              onPress={() => router.push(item.screen)}
-            >
-              <View
-                key={item.id}
-                className="flex-row items-end space-x-4 rounded-md border border-gray-200 bg-white p-1 px-8 shadow-sm shadow-gray"
-              >
-                <Image
-                  className="h-40 w-40"
-                  style={{ resizeMode: "contain" }}
-                  source={item.image}
-                />
-                <Text className="justify-center text-base font-bold">
-                  {item.title}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )
-        } else if (!item?.role) {
-          return (
-            <TouchableOpacity
-              className="bg-white p-4"
-              onPress={() => router.push(item.screen)}
-            >
-              <View
-                key={item.id}
-                className="flex-row items-end space-x-4 rounded-md border border-gray-200 bg-white p-1 px-8 shadow-sm shadow-gray"
-              >
-                <Image
-                  className="h-40 w-40"
-                  style={{ resizeMode: "contain" }}
-                  source={item.image}
-                />
-                <Text className="justify-center text-base font-bold">
-                  {item.title}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )
-        } else {
-          return <></>
+        if ((item.role && role === "ATTENDANT") || !item.role) {
+          return <HomeButton item={item} />
         }
-      }
-      }
+        return <></>
+      }}
     />
+  );
+}
+
+
+const HomeButton = ({ item }: { item: any }) => {
+  const router = useRouter()
+  return (
+    <TouchableOpacity
+      className="bg-white p-4"
+      onPress={() => router.push(item.screen)}
+    >
+      <View
+        key={item.id}
+        className="flex-row items-end space-x-4 rounded-md border border-gray-200 bg-white p-1 px-8 shadow-sm shadow-gray"
+      >
+        <Image
+          className="h-40 w-40"
+          style={{ resizeMode: "contain" }}
+          source={item.image}
+        />
+        <Text className="justify-center text-base font-bold">
+          {item.title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+
   );
 }
 
